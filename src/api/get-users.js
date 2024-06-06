59dfa4bb718ea39c248f70b74e8cd321ae488920
Nikolay <repyah84@gmail.com>
2024-06-06 03:58:39 +0300

    change cred

--- src/api/get-users.js
+++ src/api/get-users.js
@@ -16,6 +16,7 @@ export const getUsers = async (next) => {
         headers: {
           accept: "*/*",
           "accept-language": "en,en-US;q=0.9,uk;q=0.8,ru;q=0.7",
+          authorization: "Bearer guest",
           "cache-control": "no-cache",
           "content-type": "application/json",
           pragma: "no-cache",
@@ -28,7 +29,7 @@ export const getUsers = async (next) => {
           "sec-fetch-mode": "cors",
           "sec-fetch-site": "same-site",
           cookie:
-            "_hjSessionUser_665482=eyJpZCI6ImVlNTJjMDY0LTdjM2QtNThmNy04NjA5LTliYjJlZmI0ZDRlNSIsImNyZWF0ZWQiOjE3MTIwMDM2NjM3NDIsImV4aXN0aW5nIjp0cnVlfQ==; _gid=GA1.2.594253169.1712263711; timezone=Europe%2FAthens; PHPSESSID=lemi2hm0mogq8bq5r7d5kqj9ht3v1u78aklll9ah; MSG_LEG_TKN=vv2LrMQY4OZaCHRQOFMNng==; KGID=ea669cd9-4ad8-5dfe-9828-2c1335075662; _hjSession_665482=eyJpZCI6IjhlZjk0MGQxLTkwZTEtNDEzMi05NmE1LTM2NDgwNGE2NjZmNyIsImMiOjE3MTc2MDg2NjcyNDYsInMiOjAsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjowLCJzcCI6MH0=; _ga=GA1.1.1855695697.1712003663; XSRF-TOKEN=eyJpdiI6IjI0ZllNQjhvaE5yQ3JSUHFKTEtkdmc9PSIsInZhbHVlIjoiU2h4K29Id3Rzc0o1NjBjM251WW1QMVdveDV3a1M1S3VvVWZHRVRvOE1RcDNkMFo4aTRlQnllblwvWTZrME83b3ciLCJtYWMiOiJlYWEyY2FlYzQ5ZDk1NzgyNWY2N2M2YWYzOTg0ZDY0NWQxYzczNzkzNjhjZDhkMTk0ZWRiN2I0ZjQ0NDk5MzhkIn0%3D; _ga_K93D8HD50B=GS1.1.1717608665.449.1.1717609492.59.0.0",
+            "_hjSessionUser_665482=eyJpZCI6ImVlNTJjMDY0LTdjM2QtNThmNy04NjA5LTliYjJlZmI0ZDRlNSIsImNyZWF0ZWQiOjE3MTIwMDM2NjM3NDIsImV4aXN0aW5nIjp0cnVlfQ==; _gid=GA1.2.594253169.1712263711; timezone=Europe%2FAthens; MSG_LEG_TKN=vv2LrMQY4OZaCHRQOFMNng==; KGID=ea669cd9-4ad8-5dfe-9828-2c1335075662; _ga=GA1.1.1855695697.1712003663; seenWarningPage=eyJpdiI6InE1eEJiRDJycXZRSHNtXC9pM1JFNW1nPT0iLCJ2YWx1ZSI6IlNFWVhaaU1weGJEd0ExMlJtSnhkdUE9PSIsIm1hYyI6ImEwNzFjYmQzNjdkNWNmYWU5N2ZlYzY1YzlmMzMwMTU5NWViNDRkYzg4NDBkMmE5ODVkYWQyNGQyMjQxNzlkYzEifQ%3D%3D; PHPSESSID=6o263om035bgb1512mo3sg804c174gc43aflfksd; XSRF-TOKEN=eyJpdiI6Iks0RG9EbEx1QmhBU0R1eDhxU3Y3d2c9PSIsInZhbHVlIjoiQVhpbThjVmtieHY3UWNyV3k0QVZHQ3prT21NQkQ2aWFIaHJGTFNWMjBPVWUwc0JhNVJ5eU4ySUlUWmJjbzNFQyIsIm1hYyI6IjJjNTk0NzEyZDMwOWYxZWM5YTA5NzBjZDFjOWVmZDE3ODNhMjY4ODZkMTYyNGZjZDI4NDI3ZmQ3ZmE4YWM1NzQifQ%3D%3D; _ga_K93D8HD50B=GS1.1.1717634618.452.1.1717634648.30.0.0; _hjSession_665482=eyJpZCI6ImNhMTIxZjliLWFlNTEtNDJkYS1hNDg4LTk5NGQ1MzkxMGQ0MyIsImMiOjE3MTc2MzQ2NDk5MjIsInMiOjAsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjowLCJzcCI6MH0=",
           Referer: "https://www.manyvids.com/",
           "Referrer-Policy": "strict-origin-when-cross-origin",
         },
