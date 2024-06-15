4238299ee53e6fe3fa8f344ef1914cf20cc3f26a
Nikolay <repyah84@gmail.com>
2024-06-15 10:39:50 +0300

    deploy handle

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,4 +6,15 @@ module.exports = {
       exec_mode: "cluster",
     },
   ],
+
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: ["95.217.124.99"],
+      ref: "origin/master",
+      repo: "github.com/Repyah84/USER_META.git",
+      path: "/root/server/USER_META",
+      "post-deploy": "npm install",
+    },
+  },
 };
