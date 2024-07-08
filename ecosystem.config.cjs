9183dbc5d043f3048353bde35bcafa26bd2fd7bc
Nikolay <repyah84@gmail.com>
2024-07-08 11:36:36 +0300

    minor change

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       stop_exit_codes: [0],
       exec_mode: "cluster",
       cron_restart: "0 0 * * 0",
-      max_memory_restart: "3G",
+      max_memory_restart: "4G",
       node_args: "--max-old-space-size=3072",
     },
   ],
