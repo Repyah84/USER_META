77784135d27ce580781fc42c5ee6d189da635830
Nikolay <repyah84@gmail.com>
2024-07-14 08:53:09 +0300

    minor

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ const TEST = true;
 const USERS_DATA_PARS_CHUNKS = [];
 
 /** @type {number} */
-const MAX_LENGTH = TEST ? 500 : Infinity;
+const MAX_LENGTH = TEST ? 2000 : Infinity;
 
 /** @type {Array<string>} */
 const USERS_META_CACHE = [];
